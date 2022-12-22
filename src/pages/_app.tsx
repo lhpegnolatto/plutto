c006fae6755c0ab317fccc3c7207e18aa0e6d6c2
Luiz Henrique Pegnolatto <lhpegnolatto@gmail.com>
2022-12-22 20:41:11 -0300

    feat: adding plutto favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import { theme } from "theme";
@@ -6,6 +7,15 @@ import { theme } from "theme";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>Plutto</title>
+        <meta
+          name="description"
+          content="Plutto is a simple personal finance helper for organizing and planning expenses."
+        />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+
       <Component {...pageProps} />
     </ChakraProvider>
   );
