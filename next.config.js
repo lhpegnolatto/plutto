1392972d590846e5067a3d8b626d09a74369061a
Luiz Henrique Pegnolatto <lhpegnolatto@gmail.com>
2022-12-26 11:07:34 -0300

    refactor: changing pages extensions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ["public.ts", "public.tsx"],
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
