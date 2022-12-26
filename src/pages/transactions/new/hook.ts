7d6d77e227447752b983450ba1b1bab59470965c
Luiz Henrique Pegnolatto <lhpegnolatto@gmail.com>
2022-12-26 17:11:55 -0300

    fix: adding supabase types on eslint ignore

--- src/pages/transactions/new/hook.ts
+++ src/pages/transactions/new/hook.ts
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useSupabaseClient, useUser } from "@supabase/auth-helpers-react";
 
-import { Database } from "types/supabase";
+import { Database } from "types/supabase.types";
 
 type CategoryOption = {
   value: string;
