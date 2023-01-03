13eedd7755a8800d44911f520476b3d897c38c64
Luiz Henrique Pegnolatto <lhpegnolatto@gmail.com>
2023-01-03 18:16:47 -0300

    feat: adding google as a service to sign in

--- src/pages/sign-in.public.tsx
+++ src/pages/sign-in.public.tsx
@@ -1,9 +1,10 @@
-import { Button, Flex, Heading, Icon } from "@chakra-ui/react";
+import { Button, Flex, Heading, HStack, Icon } from "@chakra-ui/react";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useAppLoaderContext } from "contexts/AppLoaderContext";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { FiGithub } from "react-icons/fi";
+import { IoLogoGoogle } from "react-icons/io";
 
 import { NextPageWithLayout } from "./_app.public";
 
@@ -13,11 +14,11 @@ const AuthPage: NextPageWithLayout = () => {
 
   const { isAppLoading, setIsAppLoading } = useAppLoaderContext();
 
-  async function handleSignIn() {
+  async function handleSignIn(provider: "github" | "google") {
     setIsAppLoading(true);
 
     await supabaseClient.auth.signInWithOAuth({
-      provider: "github",
+      provider,
       options: {
         redirectTo:
           process.env.NEXT_PUBLIC_SITE_URL ??
@@ -56,14 +57,23 @@ const AuthPage: NextPageWithLayout = () => {
         Please sign in using one of this services
       </Heading>
 
-      <Button
-        onClick={handleSignIn}
-        leftIcon={<Icon as={FiGithub} />}
-        mt="4"
-        isLoading={isAppLoading}
-      >
-        Github
-      </Button>
+      <HStack spacing="4" mt="4">
+        <Button
+          onClick={() => handleSignIn("github")}
+          leftIcon={<Icon as={FiGithub} />}
+          isLoading={isAppLoading}
+        >
+          Github
+        </Button>
+        <Button
+          onClick={() => handleSignIn("google")}
+          leftIcon={<Icon as={IoLogoGoogle} />}
+          isLoading={isAppLoading}
+          colorScheme="orange"
+        >
+          Google
+        </Button>
+      </HStack>
     </Flex>
   );
 };
