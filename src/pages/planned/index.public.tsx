62751886e3a8d83135bacac3564fb0384cdcd4c0
Luiz Henrique Pegnolatto <lhpegnolatto@gmail.com>
2023-01-30 18:00:53 -0300

    feat: removing old planned page

--- src/pages/planned/index.public.tsx
+++
@@ -1,9 +0,0 @@
-import { Flex, Heading } from "@chakra-ui/react";
-
-export default function Planned() {
-  return (
-    <Flex as="main" h="full" alignItems="center" justifyContent="center">
-      <Heading as="h1">Planned page</Heading>
-    </Flex>
-  );
-}
