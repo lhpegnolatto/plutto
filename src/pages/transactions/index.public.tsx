f568dcec4839654a08072c16a7ce51a671f8c304
Luiz Henrique Pegnolatto <lhpegnolatto@gmail.com>
2023-02-20 09:26:57 -0300

    feat: adjusting style of transactions list

--- src/pages/transactions/index.public.tsx
+++ src/pages/transactions/index.public.tsx
@@ -4,24 +4,21 @@ import {
   Button,
   Card,
   CardBody,
+  Circle,
+  Divider,
   Flex,
   Heading,
   HStack,
   Icon,
+  IconButton,
   Skeleton,
   Stack,
   Stat,
   StatHelpText,
   StatLabel,
   StatNumber,
-  Table,
-  TableContainer,
   Tag,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
+  Text,
 } from "@chakra-ui/react";
 import { FiFilter } from "react-icons/fi";
 
@@ -29,7 +26,13 @@ import { NextPageWithLayout } from "pages/_app.public";
 
 import { routes } from "constants/routes";
 import { TransactionItem, useTransactions } from "./hook";
-import { transactionTypes } from "constants/transactionTypes";
+import {
+  HiArrowTrendingDown,
+  HiArrowTrendingUp,
+  HiOutlinePencil,
+  HiOutlineTrash,
+} from "react-icons/hi2";
+import { ConfirmationAlertDialog } from "components/ConfirmationAlertDialog";
 
 const Transactions: NextPageWithLayout = () => {
   const {
@@ -50,8 +53,6 @@ const Transactions: NextPageWithLayout = () => {
         return `Repeat ${frequency}`;
       case "installment_based":
         return `Installment ${installment_label}`;
-      case "unique":
-        return "Unique";
       default:
         return "";
     }
@@ -87,7 +88,12 @@ const Transactions: NextPageWithLayout = () => {
             <CardBody>
               <Stat>
                 <StatLabel>Saved Money</StatLabel>
-                <StatNumber>${summary.savedMoneyAmount}</StatNumber>
+                <StatNumber>
+                  {new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  }).format(summary.savedMoneyAmount)}
+                </StatNumber>
                 <StatHelpText>
                   {formattedStartDate} - {formattedEndDate}
                 </StatHelpText>
@@ -100,7 +106,12 @@ const Transactions: NextPageWithLayout = () => {
             <CardBody>
               <Stat>
                 <StatLabel>Revenues</StatLabel>
-                <StatNumber>${summary.revenuesAmount}</StatNumber>
+                <StatNumber>
+                  {new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  }).format(summary.revenuesAmount)}
+                </StatNumber>
                 <StatHelpText>
                   {formattedStartDate} - {formattedEndDate}
                 </StatHelpText>
@@ -113,7 +124,12 @@ const Transactions: NextPageWithLayout = () => {
             <CardBody>
               <Stat>
                 <StatLabel>Expenses</StatLabel>
-                <StatNumber>${summary.expensesAmount}</StatNumber>
+                <StatNumber>
+                  {new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  }).format(summary.expensesAmount)}
+                </StatNumber>
                 <StatHelpText>
                   {formattedStartDate} - {formattedEndDate}
                 </StatHelpText>
@@ -123,101 +139,148 @@ const Transactions: NextPageWithLayout = () => {
         </Skeleton>
       </Stack>
 
-      <Heading as="h2" fontSize="md" mt="12">
-        List of all transactions in this time
+      <Heading as="h2" fontSize="md" mt="12" mb="3" color="gray.400">
+        Feb 2023
       </Heading>
 
-      <TableContainer mt="6">
-        <Table variant="simple" size="sm">
-          <Thead>
-            <Tr>
-              <Th w="px">Type</Th>
-              <Th>Description</Th>
-              <Th>Recurrence</Th>
-              <Th isNumeric>Amount</Th>
-              <Th>Category</Th>
-              <Th>Payment method</Th>
-              <Th>Transacted at</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {transactions.map((transaction) => {
-              const {
-                id,
-                purpose,
-                description,
-                amount,
-                category_color,
-                category_title,
-                payment_method_color,
-                payment_method_title,
-                occurred_at,
-              } = transaction;
+      <Flex as="ul" gap="3" flexDirection="column">
+        {transactions.map((transaction) => {
+          const {
+            id,
+            purpose,
+            description,
+            amount,
+            category_color,
+            category_title,
+            payment_method_color,
+            payment_method_title,
+            occurred_at,
+          } = transaction;
 
-              return (
-                <Tr key={id}>
-                  <Td>
-                    <Tag colorScheme={transactionTypes[purpose]?.colorScheme}>
-                      {transactionTypes[purpose]?.label}
-                    </Tag>
-                  </Td>
-                  <Td>{description}</Td>
-                  <Td>{getTransactionRecurrenceColumn(transaction)}</Td>
-                  <Td isNumeric>${amount}</Td>
-                  <Td>
-                    <Tag colorScheme={category_color}>{category_title}</Tag>
-                  </Td>
-                  <Td>
-                    {payment_method_title && (
-                      <Tag colorScheme={payment_method_color}>
-                        {payment_method_title}
+          const recurrenceText = getTransactionRecurrenceColumn(transaction);
+
+          return (
+            <Card
+              key={id}
+              py="4"
+              px="6"
+              w="full"
+              flexDirection="row"
+              alignItems="center"
+              justifyContent="space-between"
+              gap="3"
+              as="li"
+            >
+              <Flex alignItems="center" gap="3" h="full">
+                <Icon
+                  as={
+                    purpose === "expense"
+                      ? HiArrowTrendingDown
+                      : HiArrowTrendingUp
+                  }
+                  color={purpose === "expense" ? "red.400" : "green.300"}
+                  boxSize="6"
+                />
+
+                <Divider orientation="vertical" height="8" />
+
+                <Flex flexDirection="column" gap="1">
+                  <Flex alignItems="center" gap="2">
+                    <Text fontSize="sm">{description}</Text>
+
+                    <Circle size="4px" bg="gray.500" />
+
+                    <Flex gap="2">
+                      <Tag
+                        colorScheme={category_color}
+                        size="sm"
+                        lineHeight="none"
+                      >
+                        {category_title}
                       </Tag>
-                    )}
-                  </Td>
-                  <Td>{occurred_at}</Td>
-                </Tr>
-              );
-            })}
-            {!isTransactionsLoading && transactions.length === 0 && (
-              <Tr>
-                <Td colSpan={7}>
-                  <Flex
-                    py="4"
-                    flexDirection="column"
-                    alignItems="center"
-                    justifyContent="center"
-                    fontSize="md"
-                  >
-                    {"You don't have any transaction yet :("}
+                      {payment_method_title && (
+                        <Tag
+                          colorScheme={payment_method_color}
+                          size="sm"
+                          lineHeight="none"
+                        >
+                          {payment_method_title}
+                        </Tag>
+                      )}
+                    </Flex>
                   </Flex>
-                </Td>
-              </Tr>
-            )}
-            {isTransactionsLoading && (
-              <Tr>
-                <Td>
-                  <Skeleton height="25px" w="100%" />
-                </Td>
-                <Td>
-                  <Skeleton height="25px" w="100%" />
-                </Td>
-                <Td>
-                  <Skeleton height="25px" w="100%" />
-                </Td>
-                <Td>
-                  <Skeleton height="25px" w="100%" />
-                </Td>
-                <Td>
-                  <Skeleton height="25px" w="100%" />
-                </Td>
-                <Td>
-                  <Skeleton height="25px" w="100%" />
-                </Td>
-              </Tr>
-            )}
-          </Tbody>
-        </Table>
-      </TableContainer>
+
+                  <Flex alignItems="center" gap="2">
+                    <Text color="gray.500" fontSize="xs">
+                      {occurred_at}
+                    </Text>
+
+                    {recurrenceText && (
+                      <>
+                        <Circle size="4px" bg="gray.500" />
+
+                        <Text color="gray.500" fontSize="xs">
+                          {recurrenceText}
+                        </Text>
+                      </>
+                    )}
+                  </Flex>
+                </Flex>
+              </Flex>
+
+              <Flex alignItems="center" gap="4">
+                <Text
+                  fontSize="sm"
+                  textAlign="end"
+                  color={purpose === "expense" ? "red.400" : "green.300"}
+                >
+                  {new Intl.NumberFormat("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  }).format(amount * (purpose === "expense" ? -1 : 1))}
+                </Text>
+
+                <Divider orientation="vertical" height="8" />
+
+                <Flex gap="2">
+                  <IconButton
+                    aria-label="Edit category"
+                    icon={<Icon as={HiOutlinePencil} />}
+                    size="xs"
+                  />
+                  <ConfirmationAlertDialog
+                    onConfirm={() => {}}
+                    confirmButtonText="Delete"
+                  >
+                    {(onClick) => (
+                      <IconButton
+                        aria-label="Delete category"
+                        icon={<Icon as={HiOutlineTrash} />}
+                        size="xs"
+                        onClick={onClick}
+                      />
+                    )}
+                  </ConfirmationAlertDialog>
+                </Flex>
+              </Flex>
+            </Card>
+          );
+        })}
+        {!isTransactionsLoading && transactions.length === 0 && (
+          <Card py="4" px="6" w="full" alignItems="center">
+            <Text fontSize="md">{"You don't have any transaction yet :("}</Text>
+          </Card>
+        )}
+        {isTransactionsLoading && (
+          <>
+            <Skeleton height="75px" w="full" />
+            <Skeleton height="75px" w="full" />
+            <Skeleton height="75px" w="full" />
+            <Skeleton height="75px" w="full" />
+            <Skeleton height="75px" w="full" />
+          </>
+        )}
+      </Flex>
     </Box>
   );
 };
