e24dca3d0713c5f97a51b439d6298b0f455af516
Luiz Henrique Pegnolatto <lhpegnolatto@gmail.com>
2023-03-27 20:25:30 -0300

    fix: fixing home quote

--- src/pages/home/components/CalendarCard/index.tsx
+++ src/pages/home/components/CalendarCard/index.tsx
@@ -1,8 +1,15 @@
 import { Card, Flex, Text, VStack } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { calendarCardQuotes } from "./data";
 
 export function CalendarCard() {
+  const quote = useMemo(() => {
+    const randomIndex = Math.floor(Math.random() * calendarCardQuotes.length);
+    const randomQuote = calendarCardQuotes[randomIndex];
+
+    return randomQuote;
+  }, []);
+
   const [today, setToday] = useState(new Date());
 
   const day = today.getDate();
@@ -25,9 +32,6 @@ export function CalendarCard() {
     };
   }, []);
 
-  const randomIndex = Math.floor(Math.random() * calendarCardQuotes.length);
-  const randomQuote = calendarCardQuotes[randomIndex];
-
   return (
     <Card>
       <Flex px="6" pt="6" w="full" justifyContent="space-between">
@@ -48,10 +52,10 @@ export function CalendarCard() {
       </Flex>
       <Flex px="6" pb="6" w="full" flexDir="column" alignItems="center" gap="1">
         <Text lineHeight="none" color="gray.400" textAlign="center">
-          {randomQuote.quote}
+          {quote.text}
         </Text>
         <Text fontSize="sm" lineHeight="none" color="gray.500">
-          {`- ${randomQuote.author}`}
+          {`- ${quote.author}`}
         </Text>
       </Flex>
     </Card>
