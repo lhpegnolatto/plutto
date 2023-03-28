18a99a29a41b7a79cc63df7acb5832e3a349f418
Luiz Henrique Pegnolatto <lhpegnolatto@gmail.com>
2023-03-28 07:06:27 -0300

    style: adjusting fast insights skeletons

--- src/pages/home/components/FastInsightsCard/index.tsx
+++ src/pages/home/components/FastInsightsCard/index.tsx
@@ -1,18 +1,13 @@
-import {
-  Card,
-  Flex,
-  GridItem,
-  Skeleton,
-  Text,
-  VStack,
-} from "@chakra-ui/react";
+import { Box, Card, Flex, Grid, GridItem, Text } from "@chakra-ui/react";
 
 export function FastInsightsCard() {
   return (
     <GridItem colSpan={{ base: 1, lg: 2 }}>
       <Card h="full" position="relative">
         <Flex alignItems="center" p="6" gap="3">
-          <Text fontWeight="semibold">Fast insights</Text>
+          <Text fontWeight="semibold" filter="blur(2px)">
+            Fast insights
+          </Text>
         </Flex>
         <Text
           position="absolute"
@@ -21,17 +16,24 @@ export function FastInsightsCard() {
           transform="translate(-50%, -50%)"
           zIndex={1}
           fontSize="xl"
+          bg="whiteAlpha.400"
+          px="4"
+          py="2"
+          borderRadius="md"
         >
           coming soon
         </Text>
 
-        <VStack h="full" p="6" filter="blur(2px)">
-          <Skeleton fadeDuration={10} speed={2} height="20px" width="full" />
-          <Skeleton fadeDuration={10} speed={2} height="40px" width="full" />
-          <Skeleton fadeDuration={10} speed={2} height="20px" width="full" />
-          <Skeleton fadeDuration={10} speed={2} height="20px" width="full" />
-          <Skeleton fadeDuration={10} speed={2} height="60px" width="full" />
-        </VStack>
+        <Flex filter="blur(4px)" p="6" gap="4">
+          <Grid h="full" w="50%" gap="4" templateRows="1fr 2fr 1fr 1fr 3fr">
+            <Box borderRadius="md" h="20px" w="full" bg="gray.600" />
+            <Box borderRadius="md" h="40px" w="full" bg="gray.600" />
+            <Box borderRadius="md" h="20px" w="full" bg="gray.600" />
+            <Box borderRadius="md" h="20px" w="full" bg="gray.600" />
+            <Box borderRadius="md" h="60px" w="full" bg="gray.600" />
+          </Grid>
+          <Box h="100%" w="50%" bg="gray.600" borderRadius="md" />
+        </Flex>
       </Card>
     </GridItem>
   );
